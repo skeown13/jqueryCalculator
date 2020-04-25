bc3e1f936c26a9806f9903c2231d1a628a9d5528
skeown13 <code@keown.net>
2020-04-25 12:37:11 -0700

    psuedocode added to the js file

---
+++ assets/javascript/functionality.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+// Variables to store numbers/operators
+
+// Variables to grab the id from HTML
+
+// If assignment operator is clicked first, nothing happens
+
+// First number(s) clicked get placed in #first-number and stored in a variable as a number
+
+// After some numbers are entered, when an operator is selected, store that in #operator and in it's variable -- the operator may be changed at any point after this
+
+// After an operator is chosen, any additional numbers will be placed in # second-number and stored in a varable as a number -- no new numbers may be added to #first-number
+
+// When the equal button is clicked, it will do the math operation of the operator stored in #operator of the #first-number and #second-number
+
+// Make the #clear-button clear all variables and the result card
+
+
+
+
+
+
+
+})
