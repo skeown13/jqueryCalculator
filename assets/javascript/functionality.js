6649393b19782ce302fa8b72e75b45b9dee9e58c
skeown13 <code@keown.net>
2020-04-28 18:31:15 -0700

    refactored the firstNumber so that it uses the class of number and the .val() property

--- assets/javascript/functionality.js
+++ assets/javascript/functionality.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 
 // Variables to store numbers/operators
 
-  let firstNum;
   let firstFullNum = "";
   let op;
   let secondNum;
@@ -20,42 +19,11 @@ $(document).ready(function() {
 
 // First number(s) clicked get placed in #first-number and stored in a variable as a number
 
-  $("#button-0, #button-1, #button-2, #button-3, #button-4, #button-5, #button-6, #button-7, #button-8, #button-9").on("click", function() {
+  $(".number").on("click", function() {
 
     if(op === undefined) {
-      if (this.id === "button-0") {
-        firstFullNum = firstFullNum + "0";
-        firstNum = 0;
-      } else if (this.id === "button-1") {
-        firstFullNum = firstFullNum + "1";
-        firstNum = 1;
-      } else if (this.id === "button-2") {
-        firstFullNum = firstFullNum + "2";
-        firstNum = 2;
-      } else if (this.id === "button-3") {
-        firstFullNum = firstFullNum + "3";
-        firstNum = 3;
-      } else if (this.id === "button-4") {
-        firstFullNum = firstFullNum + "4";
-        firstNum = 4;
-      } else if (this.id === "button-5") {
-        firstFullNum = firstFullNum + "5";
-        firstNum = 5;
-      } else if (this.id === "button-6") {
-        firstFullNum = firstFullNum + "6";
-        firstNum = 6;
-      } else if (this.id === "button-7") {
-        firstFullNum = firstFullNum + "7";
-        firstNum = 7;
-      } else if (this.id === "button-8") {
-        firstFullNum = firstFullNum + "8";
-        firstNum = 8;
-      } else if (this.id === "button-9") {
-        firstFullNum = firstFullNum + "9";
-        firstNum = 9;
-      };
-
-      firstNumber.append(firstNum);
+      firstFullNum += $(this).val();
+      firstNumber.text(firstFullNum);
 
     } else if(op !== undefined && finalResult === false) { 
       if (this.id === "button-0") {
@@ -98,7 +66,7 @@ $(document).ready(function() {
 // After some numbers are entered, when an operator is selected, store that in #operator and in it's variable -- the operator may be changed at any point after this
 
   $("#button-plus, #button-minus, #button-multiply, #button-divide, #button-power").on("click", function() {
-    if(firstNum !== undefined) {
+    if(firstFullNum !== undefined) {
       if(this.id === "button-plus") {
         op = "+";
       } else if (this.id === "button-minus") {
@@ -146,7 +114,6 @@ $(document).ready(function() {
     operator.empty();
     secondNumber.empty();
     result.empty();
-    firstNum = undefined;
     op = undefined;
     secondNum = undefined;
     firstFullNum = "";
